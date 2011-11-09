b43fb87685b09d6060411b3386dbec8478b34226
Jason Peak <jpeak5@lsu.edu>
2011-11-08 19:47:49 -0600

    alright, I think this is the last bit of skeleton that we discussed earlier today in Middleton

--- PrefixMatcher.java
+++ PrefixMatcher.java
@@ -3,10 +3,27 @@ public class PrefixMatcher {
 
 	/**
 	 * @param args
+	 * this program takes a file as command line parameter
+	 * <br/>This file should contain a number of strings within which we will search for prefixes<br/>
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Trie t = new Trie();
+		Array a = new Array();
+		//while((args[0].hasNextLine() = l)!=null){
+			//t.insert(l);
+			//a.insert(l);
+			//done
+		//}
+		while(true){
+			//System.out.print("$prefixMatcher->");//print a prompt
+			//wait for input
+			//when input comes,
+			//start the timer
+			//call a.search(p), and save the returned List<String> for later output
+			//call t.search(p), and save the returned List<String> for later output
+			//output the search results
+			//stop the timer
+		}
 	}
-
+	
 }
